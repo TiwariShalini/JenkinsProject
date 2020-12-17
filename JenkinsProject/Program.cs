a03cb3b70f4c96281e34f190a37d06133278428e
TiwariShalini <76032610+TiwariShalini@users.noreply.github.com>
2020-12-17 13:27:15 +0530

    after build1

--- JenkinsProject/Program.cs
+++ JenkinsProject/Program.cs
@@ -11,6 +11,7 @@ namespace JenkinsProject
         static void Main(string[] args)
         {
             Console.WriteLine("Jenkins");
+            Console.WriteLine("1st");
         }
        
     }
